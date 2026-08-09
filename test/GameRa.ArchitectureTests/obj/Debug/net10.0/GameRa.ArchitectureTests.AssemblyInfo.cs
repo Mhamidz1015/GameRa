de9c30f5bc0401bbd6d47a8201294472fd2387c1
Mhamidz <mhamidz1015@gmail.com>
2026-08-09 20:55:31 +0330

    fix final

--- test/GameRa.ArchitectureTests/obj/Debug/net10.0/GameRa.ArchitectureTests.AssemblyInfo.cs
+++ test/GameRa.ArchitectureTests/obj/Debug/net10.0/GameRa.ArchitectureTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameRa.ArchitectureTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d119b6226f90a7e713422d78d52a7f858413bca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6de029f7e851caea445f4d893a20abd0a5f7c2da")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameRa.ArchitectureTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameRa.ArchitectureTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
